0173c4a6aecd63f57224ba5e54eeaff88bd0d4ad
GrantWoood <grant_wood@outlook.com>
2024-08-31 10:55:09 +0800

    add procedure for configure and load profile

--- AsCore/AsApplication.cs
+++ AsCore/AsApplication.cs
@@ -1,6 +1,7 @@
 using AsAbstract;
 using AsBasic;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace AsCore;
 
@@ -10,19 +11,18 @@ public class AsApplication
     public IIoServiceFactory? IoServiceFactory { get; set; } = null;
     public List<IIoService> IoServices => _ioServices;
     public ISyncManager? SyncManager{get;set;}
+    public required ILogger Logger{ get; set; }
+    private IConfiguration? _configuration;
+    private IConfiguration? _profile;
 
     /**
     加载默认配置
     */
     public bool Configure(IConfiguration? configuration)
     {
-        var service = IoServiceFactory!.Create("");
-        if (service != null)
-        {
-            service.Configure(null);
-            _ioServices.Add(service);
-        }
-        return false;
+        Logger.LogInformation("Application configured");
+        _configuration = configuration;
+        return true;
     }
 
     /**
@@ -51,7 +51,41 @@ public class AsApplication
     /**
     加载配置信息，如设备设置、通道设置，分析模式等
     */
-    public bool LoadProfile(){
+    public bool LoadProfile(IConfiguration? profile){
+        if(profile == null){
+            //Load Demo
+            Logger.LogInformation("Profile isn't provided, use default profile!");
+            var service = IoServiceFactory!.Create("");
+            if (service != null)
+            {
+                service.Configure(null);
+                _ioServices.Add(service);
+            }
+        }
+        else
+        {
+            var servicesProfile = profile.GetSection("IoServices");
+            foreach (var sp in servicesProfile.GetChildren())
+            {
+                var sId = sp["Id"];
+                var service = IoServiceFactory!.Create(sId);
+                if (service == null)
+                {
+                    Logger.LogError($"Profile is provided, but io service create failed for {sId}");
+                    //Service created failed
+                }
+                else if (!service.LoadProfile(sp))
+                {
+                    Logger.LogError($"Load profile failed for io service {service.Name}!");
+                }
+                else
+                {
+                    Logger.LogInformation($"Profile load success for {service.Name}!");
+                }
+            }
+        }
+
         return true;
     }
+
 }
